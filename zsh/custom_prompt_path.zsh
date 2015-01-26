2af1d044d9e2b76e0963ddc7c513f93b2bd30af2
ay65535 <ay65535@icloud.com>
2015-01-26 18:37:19 +0900

    Speed up

--- zsh/custom_prompt_path.zsh
+++ zsh/custom_prompt_path.zsh
@@ -1,4 +1,4 @@
 #Load themes from yadr and from user's custom prompts (themes) in ~/.zsh.prompts
-autoload promptinit
+#autoload promptinit
 #fpath=($HOME/.yadr/zsh/prezto-themes(N-/) $HOME/.zsh.prompts(N-/) $fpath)
-promptinit
+#promptinit
