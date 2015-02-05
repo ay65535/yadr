dfbae4f2928c2db955f1d995592d87180ac95dac
ay65535 <ay65535@icloud.com>
2015-02-05 20:37:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
     #git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
 
-    git clone --depth=1 https://github.com/ay65535/dotfiles.git "$HOME/Dropbox/yadr" || echo "Error occured. exiting.." && read && exit 1
+    git clone --depth=1 https://github.com/ay65535/yadr.git "$HOME/Dropbox/yadr" || echo "Error occured. exiting.." && read && exit 1
     cd "$HOME"
     ln -sv Dropbox/yadr .yadr || echo "Error occured. exiting.." && read && exit 1
     cd "$HOME/.yadr"
