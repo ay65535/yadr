d49562c992e36fe10346d2a82ce515d18a4558c6
ay65535 <ay65535@icloud.com>
2015-03-26 17:43:39 +0900

    Small fix
    Update prezto

--- link_from_conf.sh
+++ link_from_conf.sh
@@ -15,6 +15,10 @@ while read LINE; do
     #echo
     #echo "i=$i"
     #echo "LINE=$LINE"
+    if [[ "${LINE%%\#*}" == "" ]]; then
+        echo "skip"
+        continue
+    fi
     CMD=$(echo $LINE | awk -F',' '{gsub("\\ ", " "); STR=$1; gsub(" ", "\\ "); printf("DST=`readlink %s` && [ \"$DST\" != \"%s\" ] && '"$LN"' -vsn --backup=t %s %s; ls -L %s >/dev/null || echo '"$i"'\n", $2, STR, $1, $2, $2)}')
     #echo $CMD
     eval $CMD
