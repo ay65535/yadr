6ef1fb75dbbfd0e2969aca6ea44b9de1cfd43b5c
ay65535 <ay65535@icloud.com>
2015-04-10 22:20:03 +0900

    Update link_from_conf.sh (+1 squashed commit) Squashed commits: [29673e0] Update prezto

--- link_from_conf.sh
+++ link_from_conf.sh
@@ -2,7 +2,7 @@
 
 if which gln >/dev/null; then
     LN=gln
-elif which ln >/dev/null | grep -o 'usr|opt|local'; then
+elif ln --version | grep 'GNU' > /dev/null; then
     LN=ln
 else
     echo 'GNU ln not installed'
@@ -19,7 +19,7 @@ while read LINE; do
         echo "skip"
         continue
     fi
-    CMD=$(echo $LINE | awk -F',' '{gsub("\\ ", " "); STR=$1; gsub(" ", "\\ "); printf("DST=`readlink %s` && [ \"$DST\" != \"%s\" ] && '"$LN"' -vsn --backup=t %s %s; ls -L %s >/dev/null || echo '"$i"'\n", $2, STR, $1, $2, $2)}')
+    CMD=$(echo $LINE | awk -F',' '{gsub("\\ ", " "); STR=$1; gsub(" ", "\\ "); printf("DST=`readlink %s`; [ \"$DST\" != \"%s\" ] && '"$LN"' -vsn --backup=t %s %s; ls -L %s >/dev/null || echo '"$i"'\n", $2, STR, $1, $2, $2)}')
     #echo $CMD
     eval $CMD
 done < install.conf.csv
