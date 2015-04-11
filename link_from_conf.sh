4413078a8a93bcff4865ca92448eff9168df1b08
ay65535 <ay65535@icloud.com>
2015-04-11 11:44:59 +0900

    Fix install.conf.sh and Add install.conf.cloud.csv for cloud nodes (+2 squashed commits) Squashed commits: [416d502] Update prezto [cde61c9] Update prezto

--- link_from_conf.sh
+++ link_from_conf.sh
@@ -16,10 +16,10 @@ while read LINE; do
     #echo "i=$i"
     #echo "LINE=$LINE"
     if [[ "${LINE%%\#*}" == "" ]]; then
-        echo "skip"
+        echo "skip $LINE"
         continue
     fi
-    CMD=$(echo $LINE | awk -F',' '{gsub("\\ ", " "); STR=$1; gsub(" ", "\\ "); printf("DST=`readlink %s`; [ \"$DST\" != \"%s\" ] && '"$LN"' -vsn --backup=t %s %s; ls -L %s >/dev/null || echo '"$i"'\n", $2, STR, $1, $2, $2)}')
+    CMD=$(echo $LINE | awk -F',' '{gsub("\\ ", " "); STR=$1; gsub(" ", "\\ "); printf("DST=`readlink %s`; [ \"$DST\" != \"%s\" ] && '"$LN"' -snfv --backup=t %s %s; ls -L %s >/dev/null || echo '"$i"'\n", $2, STR, $1, $2, $2)}')
     #echo $CMD
     eval $CMD
-done < install.conf.csv
+done
